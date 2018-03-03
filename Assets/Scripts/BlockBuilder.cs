c6c25e57065018800a6595cb2c4751c3d652a87a
ElasticSea <info@elasticsea.com>
2018-03-03 07:37:31 +0100

    Camera follows last block vertical position.

---
+++ Assets/Scripts/BlockBuilder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class BlockBuilder : MonoBehaviour
+    {
+        public IEnumerable<Collider> Blocks => Enumerable.Empty<Collider>();
+
+        public event Action<Collider> OnBlockAdded = block => { };
+    }
+}
---
+++ Assets/Scripts/BlockHorizontalPosition.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class BlockHorizontalPosition : MonoBehaviour
+    {
+        [SerializeField] private BlockBuilder builder;
+
+        private void Awake()
+        {
+            builder.OnBlockAdded += block =>
+            {
+                transform.position += Vector3.up * block.bounds.size.y;
+            };
+        }
+    }
+}
