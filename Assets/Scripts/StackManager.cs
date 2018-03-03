0dc141422c36110d5c024ddc62c2a0a5cdfb02cc
ElasticSea <info@elasticsea.com>
2018-03-03 08:06:41 +0100

    Spawn blocks and transition between two positions.

---
+++ Assets/Scripts/StackManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class StackManager : MonoBehaviour
+    {
+        [SerializeField] private BlockBuilder builder;
+
+        private void Start()
+        {
+            builder.SpawnBlock(Vector3.forward, Vector3.back, new Vector3(1, .2f, 1), 1);
+        }
+    }
+}
