57df0cd3876a596f7ff32f06b46a1233eb8f8ea4
ElasticSea <info@elasticsea.com>
2018-03-05 13:47:37 +0100

    Adding ui.

---
+++ Assets/Scripts/UiManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    public class UiManager : MonoBehaviour
+    {
+        [SerializeField] private StackBuilder stack;
+        [SerializeField] private Text score;
+
+        private void Awake()
+        {
+            stack.OnBlockAdded += result =>
+            {
+                score.text = stack.Blocks.Count.ToString();
+            };
+        }
+    }
+}
